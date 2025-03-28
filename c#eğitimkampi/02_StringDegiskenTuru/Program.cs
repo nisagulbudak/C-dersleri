2e520ea5449ada8f5d3f4965128999e2a3248c66
nisagulbudak <nisagb1234@gmail.com>
2025-03-28 23:35:13 +0300

    Add files via upload

---
+++ c#eğitimkampi/02_StringDegiskenTuru/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_StringDegiskenTuru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DEĞİŞKEN TÜRLERİ
+
+            #region String
+
+            //string
+            //Değişken_türü değişken_adı;
+            //alfabetik türde verileri tutabilmek için kullanılan değişken türü
+            //string name;
+            //name = "Nisa";
+            //Console.WriteLine(name);
+            //çift tırnak içerisinde yazılmaz name değerinin karşılığını istediğimiz için direkt yazılır.
+
+
+            string customerName;
+
+            //string değişkeni adlandırılırken ilk harf küçük sonraki harfler büyük yazılır.Buna Camel Case nedir
+            //değişkenler adlandırılırken araya boşluk konmaz.
+            //değişken adlandırılırken yalın ve kodlar rahat okunacak formatta yazılmalı
+
+            string customerSurname;
+            string customerPhone;
+            string customerEmail, district, city;
+
+            //birden fazla değişken tanımlanabiliyor, 30. satırda aynı string komutuna birden fazla değişken tanımladık.
+
+            customerName = "Gökberk";
+            customerSurname = "Anıt";
+            customerPhone = "+90 500 400 30 40";
+            customerEmail = "deneme@gmail.com";
+            district = "Torbalı";
+            city = "İzmir";
+
+            Console.WriteLine("****Rezervasyon Kartı****");
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Müşteri : " + customerName + " " + customerSurname);
+            //müşteri ; dedik artı araya boşluk bırakmak içi iki tane "" daha ekledik bu ikisinin arası boşluk oldu
+            Console.WriteLine("İletişim : " + customerPhone);
+            Console.WriteLine("Email Adresi : " + customerEmail);
+            Console.WriteLine("Adres : " + district + "/" + city);
+            //araya koyduğumuz "/" işareti şehir ve ilçe arasında / olsun diye konuldu ve ("")'lar (+)'la birleştirildi.
+            Console.WriteLine("---------------------------------------------------------");
+
+            //şimdi bir örnek daha yapacağız
+
+            Console.WriteLine();
+
+            customerName = "Nisa Gül";
+
+            //tekrardan değişken tanımlıyoruz bu değişkenler sırasıyla komut satırına yazılacak çünkü kod sistemindeki hiyerarşi satır sayılarına göre okuma sırası belirlenecek şekildedir ilk önce yazılan satır ilk okunur.
+
+            customerSurname = "Budak";
+            customerPhone = "+90 500 400 40 50";
+            customerEmail = "deneme1@gmail.com";
+            district = "Odunpazarı";
+            city = "Eskişehir";
+
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Müşteri : " + customerName + " " + customerSurname);
+            Console.WriteLine("İletişim : " + customerPhone);
+            Console.WriteLine("Email Adresi : " + customerEmail);
+            Console.WriteLine("Adres : " + district + "/" + city);
+            Console.WriteLine("---------------------------------------------------------");
+
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
+
+
+
---
+++ c#eğitimkampi/03_IntDegiskenTuru/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_IntDegiskenTuru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Int Değişken Türü
+            //int değişkeni tam sayı türündeki değişkenlerdir.Java,pyhton,C# veya C'de d bu şekilde kullanılır
+
+            //int 
+            //int number = 24;
+            //Console.WriteLine(number);
+
+            //şimdi müşterinin yediği içtiği ve hesabı ödemesi gerektiği bir örnek yapalım
+
+            int hamburgerPrice = 300;
+            //değişkeni yazarken yine camel case e göre ilk kelimenin baş harfi büyük sonrakilerin küçük olacak şekilde yazdık!!
+            int cokePrice = 35;
+            int waterPrice = 10;
+            int friesPrice = 50;
+            int pizzaPrice = 250;
+            int lemonadePrice = 30;
+
+            Console.WriteLine("****Restorant Menü Fiyatı****");
+            Console.WriteLine();
+            Console.WriteLine("-----Hamburger Fiyatı : " + hamburgerPrice + "TL");
+            Console.WriteLine("-----Pizza Fiyatı : " + pizzaPrice + "TL");
+            Console.WriteLine("-----Kızartma Fiyatı : " + friesPrice + "TL");
+            Console.WriteLine("-----Kola Fiyatı : " + cokePrice + "TL");
+            Console.WriteLine("-----Limonata Fiyatı : " + lemonadePrice + "TL");
+            Console.WriteLine("-----Su Fiyatı : " + waterPrice + "TL");
+            Console.WriteLine();
+            Console.WriteLine("****Restorant Menü Fiyatı****");
+
+
+
+            Console.WriteLine();
+
+            //müşteri geldi belirli sayıda ürün aldı bunun hesabını yapacağız
+
+            int hamburgerCount;
+            int cokeCount;
+            int waterCount;
+            int friesCount;
+            int pizzaCount;
+            int lemonadeCount;
+
+            int totalHamburgerPrice = 0;
+            int totalCokePrice = 0;
+            int totalWaterPrice = 0;
+            int totalFriesPrice = 0;
+            int totalPizzaPrice = 0;
+            int totalLemonadePrice = 0;
+
+            hamburgerCount = 3;
+            cokeCount = 3;
+            waterCount = 2;
+            friesCount = 3;
+            pizzaCount = 0;
+            lemonadeCount = 0;
+
+            totalHamburgerPrice = hamburgerPrice * hamburgerCount;
+            totalCokePrice = cokePrice * cokeCount;
+            totalWaterPrice = waterPrice * waterCount;
+            totalFriesPrice = friesPrice * friesCount; 
+            totalPizzaPrice = pizzaPrice * pizzaCount;
+            totalLemonadePrice = lemonadePrice * lemonadeCount;
+
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Hamburger Tutarı : " + totalHamburgerPrice + "TL");
+            Console.WriteLine("Kola Tutarı : " + totalCokePrice + "TL");
+            Console.WriteLine("Su Tutarı : " + totalWaterPrice + "TL");
+            Console.WriteLine("Kızartma Tutarı : " + totalFriesPrice + "TL");
+            Console.WriteLine("Pizza Tutarı : " + totalPizzaPrice + "TL");
+            Console.WriteLine("Limonata Tutarı : " + totalLemonadePrice + "TL");
+
+            Console.WriteLine();
+
+            int totalPrice = totalHamburgerPrice + totalCokePrice + totalWaterPrice + totalFriesPrice + totalPizzaPrice + totalLemonadePrice;
+            Console.WriteLine("Toplam Ödenecek Tutar : " + totalPrice + "TL");
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
